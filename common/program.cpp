555b76692ac626294685b4ddfc191e5460105d53
Matej Gomboc <matejg03@gmail.com>
2021-02-07 16:52:38 +0100

    Minor update.

--- common/program.cpp
+++ common/program.cpp
@@ -1,4 +1,20 @@
 #include "program.h"
+#include "nop.h"
+
+Program Program::generateRandom()
+{
+    Program program;
+    // TODO !!!
+    return program;
+}
+
+Program::Program()
+{
+    m_subprograms.push_back(std::vector<std::unique_ptr<Instruction>>());
+    m_subprograms[0].push_back(std::make_unique<Nop>());
+
+    m_instruction_pointers.push_back(0);
+}
 
 void Program::execute()
 {
--- common/program.h
+++ common/program.h
@@ -11,12 +11,15 @@ public:
     static constexpr uint16_t MAX_DATA = std::numeric_limits<uint16_t>::max();
     static constexpr uint8_t MAX_SUBPROGRAMS = std::numeric_limits<uint8_t>::max();
 
+    static Program generateRandom();
+
+    Program();
     void execute();
 
 private:
     std::vector<std::vector<std::unique_ptr<Instruction>>> m_subprograms;
     std::vector<float> m_memory;
-    uint8_t m_subprogram_pointer;
+    uint8_t m_subprogram_pointer = 0;
     std::vector<uint16_t> m_instruction_pointers;
     std::vector<uint8_t> m_return_pointers;
 };
